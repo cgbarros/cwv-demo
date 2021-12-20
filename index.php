f506b1751ded28413619eabdb0ffbabd30a00196
Caio Barros <caio.barros@gmail.com>
2021-12-20 11:52:12 +0000

    fixed links in the website explanation

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <h1>CWV Demo</h1>
     
 		<p>
-		  Each one of the pages below will purposefully create bad [Core Web Vitals](https://web.dev/vitals) scores. You can check those using Google's Chrome Extension [Web Vitals](https://chrome.google.com/webstore/detail/web-vitals/ahfhijdlegdabablpippeagghigmibma).
+		  Each one of the pages below will purposefully create bad <a href="https://web.dev/vitals">Core Web Vitals</a> scores. You can check those using Google's Chrome Extension <a href="https://chrome.google.com/webstore/detail/web-vitals/ahfhijdlegdabablpippeagghigmibma">Web Vitals</a>.
 		</p>
 		<p>
 		  Note that FID is harder to replicate. You need to try to click in the button immediatelly after the page loads and it will vary from computer to computer. If you are implementing this demo, try changing the <code>busyBrowser()</code> function in the files. We used PHP only to demonstrate how server-side rendering can improve your CLS (see <code>cls_good.php</code>)
